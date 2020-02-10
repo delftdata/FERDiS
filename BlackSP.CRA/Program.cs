d8de1bd4ca91128128047407eb67a8e529380465
Marc Zwart <marc@briskey.nl>
2020-02-10 13:39:18 +0100

    oops I did it again

--- BlackSP.CRA/Program.cs
+++ BlackSP.CRA/Program.cs
@@ -70,7 +70,7 @@ namespace BlackSP.CRA
                     }
                     string instanceName = args[2];
                     int portNum = int.Parse(args[3]);
-                    string ipAddress = "192.168.178.242";//  args.Length == 5 ? args[4] : null;
+                    string ipAddress = args.Length == 5 ? args[4] : null;
                     Worker.Launch(instanceName, portNum, dataProvider, ipAddress);
                     break;
                 default:
