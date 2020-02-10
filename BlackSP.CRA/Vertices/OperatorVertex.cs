eb359f3e7b92ba0d22940d703b9eb792a60472b2
Marc Zwart <marc@briskey.nl>
2020-02-10 21:54:04 +0100

    change load generating strategy

--- BlackSP.CRA/Vertices/OperatorVertex.cs
+++ BlackSP.CRA/Vertices/OperatorVertex.cs
@@ -29,22 +29,39 @@ namespace BlackSP.CRA.Vertices
             
             var output = new VertexOutputEndpoint<IEvent>(parallelSerializer);
             AddAsyncOutputEndpoint($"output", output);
-
+            
             //TODO: remove test crap            
             SpawnLoadGeneratingThread(input, output);
+            SpawnPassthroughThread(input, output);
             Console.WriteLine("Done");
 
             return Task.CompletedTask;
+        }
+
+        private void SpawnPassthroughThread(VertexInputEndpoint<IEvent> input, VertexOutputEndpoint<IEvent> output)
+        {   //TODO: delete method once served its purpose
+            Task.Run(async () =>
+            {
+                await Task.Delay(10000);//wait 10 seconds for connections to establish etc
+                Console.WriteLine("!!! passthrough thread !!!");
+                while (true)
+                {   //go ham enqueueing events, see how fast we can go..
+                    if (!input.IsConnected || !output.IsConnected) { continue; }
+                    IEvent next = input.GetNext();
+                    if (next == null)
+                    {
+                        output.EnqueueAll(next);
+                    }
+                }
+            });
         }
 
         private void SpawnLoadGeneratingThread(VertexInputEndpoint<IEvent> input, VertexOutputEndpoint<IEvent> output)
         {   //TODO: delete method once served its purpose
             Task.Run(async () =>
             {
-
                 await Task.Delay(10000);//wait 10 seconds for connections to establish etc
-
-                Console.WriteLine("!!! Starting high event load generation !!!");
+                Console.WriteLine("!!! load generating thread !!!");
                 Random r = new Random();
                 Stopwatch sw = new Stopwatch();
                 sw.Start();
@@ -52,50 +69,29 @@ namespace BlackSP.CRA.Vertices
                 int secondCounter = 0;
                 while (true)
                 {   //go ham enqueueing events, see how fast we can go..
-                    if (!input.IsConnected || !output.IsConnected)
-                    {
-                        await Task.Delay(100);
-                        continue; //dont enqueue while not connected..
-                    }
+                    if (!input.IsConnected || !output.IsConnected) { continue; }
 
                     for (int i = 0; i < eventsPerSec; i++)
                     {
-                        if (!input.IsConnected || !output.IsConnected) { continue; }
-
-                        IEvent next = input.GetNext() ?? new SampleEvent
+                        IEvent next = new SampleEvent
                         {
                             Key = $"KeyYo {(r.NextDouble() * 1000)}",
                             Value = $"ValueYo{(r.NextDouble() * 1000)}"
                         };
-                        output.EnqueueAll(next);
+                        if (next == null)
+                        {
+                            output.EnqueueAll(next);
+                        }
                     }
 
                     int timeTillSecond = sw.ElapsedMilliseconds < 1000 ? 1000 - (int)sw.ElapsedMilliseconds : 0;
                     await Task.Delay(timeTillSecond);
 
                     secondCounter++;
-                    if(secondCounter % 5 == 0)
-                    {
-                        DoGC();
-                    }
-
-                    if(secondCounter == 30)
-                    {
-                        eventsPerSec = eventsPerSec >= 100*1000 ? eventsPerSec * 2 : eventsPerSec * 10;
-                        secondCounter = 0;
-                        Console.WriteLine($"Ramping up to {eventsPerSec} e/s");
-                    }
 
                     sw.Restart();
                 }
-                Console.WriteLine("how the fuck?");
             });
-        }
-
-        private void DoGC()
-        {
-            GC.Collect();
-            Console.WriteLine("force collected garbage");   
         }
     }
 }
